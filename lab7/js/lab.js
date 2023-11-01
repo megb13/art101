2b75ea2013d750044893ff68a51f409dd55ba1c2
megb13 <146491363+megb13@users.noreply.github.com>
2023-11-01 15:45:38 -0700

    lab 7 kinda final

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -4,3 +4,25 @@
 */
 
 //Task 1//
+// declare a variable userName and use window.prompt for input
+
+function sortUserName(){
+  var userName = window.prompt ("Hello there! Please tell me your name and I'll give you a new one!");
+  console.log("userName =", userName);
+  //split string to array
+  var nameArray = userName.split ('');
+  console.log("nameArray =", nameArray);
+  //sort the array
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort =", nameArraySort);
+  //join array back to a string
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted =", nameSorted);
+  
+  return nameSorted;
+  }
+  
+  
+  //Output and Results
+  document.writeln("Here is your much better new name: ",
+  sortUserName(), "</br>");
